0143dd33d8d061d1ab9bf12e97d985c7d411a3d1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-20 17:45:51 +0530

    add logic to app component file.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Group} from './models/group';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'JUG Tours';
+  loading = true;
+  groups: Group[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.loading = true;
+    this.http.get<Group[]>('api/groups').subscribe((data: Group[]) => {
+      this.groups = data;
+      this.loading = false;
+    });
+  }
 }
