b363cf224c03ee533bf277429af8723904660217
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-20 13:27:05 +0530

    add user class.

---
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/models/User.java
@@ -0,0 +1,20 @@
+package com.nayanthayasiru.vehicle_reservation_service.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String contactNumber;
+}
