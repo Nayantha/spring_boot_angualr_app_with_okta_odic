f79d11827e7eed3a04a75aced89760e0e4745334
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-20 13:35:10 +0530

    add data annotation.

--- src/main/java/com/nayanthayasiru/vehicle_reservation_service/models/Group.java
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/models/Group.java
@@ -3,11 +3,13 @@ package com.nayanthayasiru.vehicle_reservation_service.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "user_group")
