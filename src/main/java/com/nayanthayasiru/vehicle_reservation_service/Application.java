afb73400dfb842f6e087d36900be15062dddd69a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-20 13:15:47 +0530

    Initial Commit.

---
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/Application.java
@@ -0,0 +1,13 @@
+package com.nayanthayasiru.vehicle_reservation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nayanthayasiru/vehicle_reservation_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayanthayasiru.vehicle_reservation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
