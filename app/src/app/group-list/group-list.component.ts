b0aab7cb265cab6a80afce66fd63c208a30cb62e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 15:49:41 +0530

    add standalone angular component for group-list.

---
+++ app/src/app/group-list/group-list.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-group-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './group-list.component.html',
+  styleUrls: ['./group-list.component.css']
+})
+export class GroupListComponent {
+
+}
