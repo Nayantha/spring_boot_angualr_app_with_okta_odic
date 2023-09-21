d1e02eaf8a202fe680067cff1310e6a087892f22
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 16:31:19 +0530

    add standalone angular component for edit group.

---
+++ app/src/app/group-edit/group-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-group-edit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './group-edit.component.html',
+  styleUrls: ['./group-edit.component.css']
+})
+export class GroupEditComponent {
+
+}
