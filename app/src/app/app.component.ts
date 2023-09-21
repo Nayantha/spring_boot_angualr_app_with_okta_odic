29db017c1e8420eddba3afcf448d9f4e8c83e169
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 15:59:26 +0530

    remove data fetching login.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,25 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Group} from './models/group';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'JUG Tours';
-  loading = true;
-  groups: Group[] = [];
-
-  constructor(private http: HttpClient) {
-  }
-
-  ngOnInit() {
-    this.loading = true;
-    this.http.get<Group[]>('api/groups').subscribe((data: Group[]) => {
-      this.groups = data;
-      this.loading = false;
-    });
-  }
 }
