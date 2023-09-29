8bac179185c47e806445f9a2e7a8d57a9dd423dc
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-29 15:18:18 +0530

    add method to get all reservations of a user after today.

--- src/main/java/com/nayanthayasiru/vehicle_reservation_service/repository/ReservationRepository.java
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/repository/ReservationRepository.java
@@ -2,9 +2,19 @@ package com.nayanthayasiru.vehicle_reservation_service.repository;
 
 import com.nayanthayasiru.vehicle_reservation_service.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, String> {
     List<Reservation> findAllByNameAndEmail(String name, String email);
+
+    @Query("""
+            SELECT *
+            FROM vehicle_reservation r
+            WHERE r.name = name and r.email = email and r.date >=: date
+            """)
+    List<Reservation> findAllByNameAndEmailAfterToday(@Param("name") String name, @Param("email") String email, @Param("date") LocalDate Today);
 }
