20e038da2293ccc6e7b3a04915c60616aa71b4e0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-29 14:50:31 +0530

    move to the correct folder.

---
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/controller/ReservationController.java
@@ -0,0 +1,11 @@
+package com.nayanthayasiru.vehicle_reservation_service.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class ReservationController {
+}
