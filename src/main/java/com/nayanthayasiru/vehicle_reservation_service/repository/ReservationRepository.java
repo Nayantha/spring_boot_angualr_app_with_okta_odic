a47e231a38f8d8182a2e704bba09518a59968472
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-30 12:53:23 +0530

    remove the method with date checkers.

--- src/main/java/com/nayanthayasiru/vehicle_reservation_service/repository/ReservationRepository.java
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/repository/ReservationRepository.java
@@ -2,18 +2,16 @@ package com.nayanthayasiru.vehicle_reservation_service.repository;
 
 import com.nayanthayasiru.vehicle_reservation_service.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.sql.Date;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllByNameAndEmail(String name, String email);
 
-    @Query("""
-            SELECT r
-            FROM vehicle_reservation r
-            WHERE r.name = ?1 and r.email = ?2 and r.date >= ?3
-            """)
-    List<Reservation> findAllByNameAndEmailFromToday(String name, String email, Date Today);
+//    @Query("""
+//            SELECT r
+//            FROM vehicle_reservation r
+//            WHERE r.name = ?1 and r.email = ?2 and r.date >= CONVERT(date, SYSDATETIME())
+//            """)
+//    List<Reservation> findAllByNameAndEmailFromToday(String name, String email, Date Today);
 }
