18fb6d71bf08f2b36967a048b5f1d5752e5b13ef
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-30 11:17:11 +0530

    add reservation edit component.

---
+++ app/src/app/reservation-edit/reservation-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reservation-edit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reservation-edit.component.html',
+  styleUrls: ['./reservation-edit.component.css']
+})
+export class ReservationEditComponent {
+
+}
