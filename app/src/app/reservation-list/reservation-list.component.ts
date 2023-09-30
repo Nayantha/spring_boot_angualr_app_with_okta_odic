62621836132d23dc8988c9879c7b4b8cc663ed67
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-30 11:14:17 +0530

    add reservation list component.

---
+++ app/src/app/reservation-list/reservation-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reservation-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reservation-list.component.html',
+  styleUrls: ['./reservation-list.component.css']
+})
+export class ReservationListComponent {
+
+}
