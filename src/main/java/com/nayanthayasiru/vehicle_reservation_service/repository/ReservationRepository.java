95066c93648c4aad964fc07031a0eb7fba9b99a9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-30 21:57:46 +0530

    remove using the second method.

--- src/main/java/com/nayanthayasiru/vehicle_reservation_service/repository/ReservationRepository.java
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/repository/ReservationRepository.java
@@ -2,17 +2,16 @@ package com.nayanthayasiru.vehicle_reservation_service.repository;
 
 import com.nayanthayasiru.vehicle_reservation_service.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllByNameAndEmail(String name, String email);
 
-    @Query("""
-            SELECT r
-            FROM vehicle_reservation r
-            WHERE r.name = ?1 and r.email = ?2 and r.date >= CURDATE()
-            """)
-    List<Reservation> findAllByNameAndEmailFromToday(String name, String email);
+//    @Query("""
+//            SELECT r
+//            FROM vehicle_reservation r
+//            WHERE r.name = ?1 and r.email = ?2 and r.date >= CURDATE()
+//            """)
+//    List<Reservation> findAllByNameAndEmailFromToday(String name, String email);
 }
