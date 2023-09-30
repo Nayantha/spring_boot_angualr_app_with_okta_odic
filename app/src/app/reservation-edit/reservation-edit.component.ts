3390100b65158d1e8de0b945fdde7a7a451f6f1b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-30 11:53:38 +0530

    copy skeleton code from group edit component ts.

--- app/src/app/reservation-edit/reservation-edit.component.ts
+++ app/src/app/reservation-edit/reservation-edit.component.ts
@@ -1,13 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatIconModule } from "@angular/material/icon";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { HttpClient } from "@angular/common/http";
+import { AuthService } from "../auth/auth.service";
+import { Reservation } from "../models/reservation";
+import { map, of, switchMap } from "rxjs";
 
 @Component({
   selector: 'app-reservation-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, MatInputModule, FormsModule, MatButtonModule, RouterLink,
+    MatDatepickerModule, MatIconModule, MatNativeDateModule, MatTooltipModule
+  ],
   templateUrl: './reservation-edit.component.html',
   styleUrls: ['./reservation-edit.component.css']
 })
-export class ReservationEditComponent {
+export class ReservationEditComponent implements OnInit {
+  reservation!: Reservation;
+  feedback: any = {};
 
+  constructor(private route: ActivatedRoute, private router: Router,
+              private http: HttpClient, private auth: AuthService) {
+  }
+
+  async ngOnInit() {
+    await this.notAuthorizedRedirectToHome(this.auth, this.router);
+    this.route.params.pipe(
+      map(p => p['id']),
+      switchMap(id => {
+        if (id === 'new') {
+          return of(new Reservation());
+        }
+        return this.http.get<Reservation>(`api/reservation/${id}`);
+      })
+    ).subscribe({
+      next: reservation => {
+        this.reservation = reservation;
+        this.feedback = {};
+      },
+      error: () => {
+        this.feedback = {type: 'warning', message: 'Error loading'};
+      }
+    });
+  }
+
+  async notAuthorizedRedirectToHome(auth: AuthService, router: Router) {
+    if (!await this.auth.isAuthenticated()) {
+      await this.router.navigate(["home"])
+    }
+  }
+
+  save() {
+    const id = this.reservation.id;
+    const method = id ? 'put' : 'post';
+
+    this.http[method]<Reservation>(`/api/reservation${id ? '/' + id : ''}`, this.reservation).subscribe({
+      next: () => {
+        this.feedback = {type: 'success', message: 'Save was successful!'};
+        setTimeout(async () => {
+          await this.router.navigate(['/reservations']);
+        }, 1000);
+      },
+      error: () => {
+        this.feedback = {type: 'error', message: 'Error saving'};
+      }
+    });
+  }
+
+  async cancel() {
+    await this.router.navigate(['/reservations']);
+  }
 }
