922d67c829b1db773f0b2f443c4c26efe33a16fd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-01 19:59:01 +0530

    remove unwanted class field.

--- app/src/app/models/reservation.ts
+++ app/src/app/models/reservation.ts
@@ -2,7 +2,6 @@ export class Reservation {
   id: number | null;
   name: string;
   email: string;
-  contactNumber: string;
   date: string;
   time: string;
 
@@ -10,7 +9,6 @@ export class Reservation {
     this.id = reservation?.id || null;
     this.name = reservation?.name || '';
     this.email = reservation?.email || "";
-    this.contactNumber = reservation?.contactNumber || "";
     this.date = reservation?.date || Date();
     this.time = reservation?.time || "";
   }
