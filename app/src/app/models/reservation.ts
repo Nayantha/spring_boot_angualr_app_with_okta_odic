4c77b4702e8a3b03c907e08fb55624ac3244d5a7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-01 20:04:15 +0530

    add a few class fields.

--- app/src/app/models/reservation.ts
+++ app/src/app/models/reservation.ts
@@ -4,6 +4,9 @@ export class Reservation {
   email: string;
   date: string;
   time: string;
+  vehicle_no: string;
+  mileage: number;
+  message: string;
 
   constructor(reservation: Partial<Reservation> = {}) {
     this.id = reservation?.id || null;
@@ -11,5 +14,8 @@ export class Reservation {
     this.email = reservation?.email || "";
     this.date = reservation?.date || Date();
     this.time = reservation?.time || "";
+    this.vehicle_no = reservation?.vehicle_no || "";
+    this.mileage = reservation?.mileage || 0;
+    this.message = reservation?.message || "";
   }
 }
