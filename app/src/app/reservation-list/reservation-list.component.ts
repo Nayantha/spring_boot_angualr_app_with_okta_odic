b7ce9e8b1e077c055da9bdeeba04c121ec219c98
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-01 20:05:57 +0530

    add a few display columns.

--- app/src/app/reservation-list/reservation-list.component.ts
+++ app/src/app/reservation-list/reservation-list.component.ts
@@ -19,7 +19,7 @@ export class ReservationListComponent {
   title = 'Reservation List';
   loading = true;
   reservations: Reservation[] = [];
-  displayedColumns = ['id', 'name', 'email', 'date', 'time', 'actions'];
+  displayedColumns = ['id', 'name', 'email', 'date', 'time', 'vehicle_no', 'mileage', 'message', 'actions'];
   feedback: any = {};
 
   constructor(private http: HttpClient, private auth: AuthService, private router: Router) {
