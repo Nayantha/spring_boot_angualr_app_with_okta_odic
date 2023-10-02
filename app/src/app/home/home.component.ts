82494f53a6947c4b46669afa1723a8ecb7b32933
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-02 19:05:55 +0530

    add logic to get user from the database.

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../auth/auth.service';
 import { User } from '../models/user';
 import { RouterLink } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -16,12 +17,16 @@ export class HomeComponent implements OnInit {
   isAuthenticated!: boolean;
   user!: User;
 
-  constructor(public auth: AuthService) {
+  constructor(public auth: AuthService, private http: HttpClient) {
   }
 
   async ngOnInit() {
     this.isAuthenticated = await this.auth.isAuthenticated();
     await this.auth.getUser().subscribe(data => this.user = data);
+    this.http.get<User>('api/get_saved_user').subscribe((data: User) => {
+      this.user = data;
+    });
+
   }
 
 }
