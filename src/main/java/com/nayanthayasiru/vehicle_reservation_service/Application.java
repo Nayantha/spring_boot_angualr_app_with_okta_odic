c158943bad5cf01b3a9b38d51ce8855279b08bd4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-02 12:19:12 +0530

    add exclude filter to app.

--- src/main/java/com/nayanthayasiru/vehicle_reservation_service/Application.java
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/Application.java
@@ -1,9 +1,29 @@
 package com.nayanthayasiru.vehicle_reservation_service;
 
+import com.nayanthayasiru.vehicle_reservation_service.controller.GroupController;
+import com.nayanthayasiru.vehicle_reservation_service.models.Event;
+import com.nayanthayasiru.vehicle_reservation_service.models.Group;
+import com.nayanthayasiru.vehicle_reservation_service.repository.GroupRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+        basePackages = {"com.nayanthayasiru"},
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        value = {
+                                Group.class,
+                                GroupRepository.class,
+                                GroupController.class,
+                                Event.class
+                        }
+                )
+        }
+)
 public class Application {
 
     public static void main(String[] args) {
