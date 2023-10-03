2f274e038187942847c08a80f30a0d39e5b71ed0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-03 10:07:46 +0530

    add a new dto class.

---
+++ src/main/java/com/nayanthayasiru/vehicle_reservation_service/DTO/UserReservationDTO.java
@@ -0,0 +1,9 @@
+package com.nayanthayasiru.vehicle_reservation_service.DTO;
+
+import com.nayanthayasiru.vehicle_reservation_service.models.Reservation;
+import com.nayanthayasiru.vehicle_reservation_service.models.User;
+
+public class UserReservationDTO {
+    public Reservation reservation;
+    public User user;
+}
