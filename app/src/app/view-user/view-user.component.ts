9538ebf27ce39df812702ee063806757ccfa5f75
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-05 16:05:10 +0530

    fill out needed code.

--- app/src/app/view-user/view-user.component.ts
+++ app/src/app/view-user/view-user.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from "@angular/common/http";
+import { AuthService } from "../auth/auth.service";
+import { Router } from "@angular/router";
+import { User } from "../models/user";
 
 @Component({
   selector: 'app-view-user',
@@ -9,5 +13,19 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./view-user.component.css']
 })
 export class ViewUserComponent {
+  user!: User;
 
+  constructor(private http: HttpClient, private auth: AuthService, private router: Router) {
+  }
+
+  async ngOnInit() {
+    await this.notAuthorizedRedirectToHome(this.auth, this.router);
+    await this.auth.getUser().subscribe(data => this.user = data);
+  }
+
+  async notAuthorizedRedirectToHome(auth: AuthService, router: Router) {
+    if (!await this.auth.isAuthenticated()) {
+      await this.router.navigate(["home"])
+    }
+  }
 }
