81404f9e668e445a069e16d25c482401398ee2fa
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-05 16:02:04 +0530

    add standalone view user component.

---
+++ app/src/app/view-user/view-user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-view-user',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './view-user.component.html',
+  styleUrls: ['./view-user.component.css']
+})
+export class ViewUserComponent {
+
+}
